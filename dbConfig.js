26b0a1b8128e300305c28cfac3fb7f28c64fe9a7
Prabhkirat Singh <127061880+prabhkirat12@users.noreply.github.com>
2024-12-01 16:01:54 +1300

    Update dbConfig.js

--- dbConfig.js
+++ dbConfig.js
@@ -1,10 +1,10 @@
 var mysql = require('mysql');
 var conn = mysql.createConnection({
-    host: 'moe-mysql-app',
+    host: 'db',  // Use the Docker Compose service name for the MySQL container
     user: 'root',
-    password: 'moeroot',
-    database:'photodb',
-    port: 3311
+    password: 'my-secret-pw',  // Update the password to match the one in your Docker Compose file
+    database: 'photodb',
+    port: 3306  // Make sure the port matches the one your MySQL service is listening on inside the container
 });
 
 conn.connect(function(err) {
